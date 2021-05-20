ec0135fa7889e1acecf7030d915f00a2b22e4de9
samyIO <samerphone1994@gmail.com>
2021-05-20 14:56:21 +0200

    ZMP-Init

---
+++ src/main/java/com/zmpruefversuche/ZmPruefversucheApplication.java
@@ -0,0 +1,13 @@
+package com.zmpruefversuche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZmPruefversucheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZmPruefversucheApplication.class, args);
+    }
+
+}

f86f17aa2d3f2633af1918c3436213d840d1b626
samyIO <samerphone1994@gmail.com>
2021-05-20 15:51:42 +0200

    testcommit

---
+++ src/main/java/com/zmp/app/ZmpApplication.java
@@ -0,0 +1,13 @@
+package com.zmp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZmpApplication.class, args);
+    }
+
+}

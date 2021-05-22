ea637ab2039a5d3d4fe6531a3b98133f6261fe87
samyIO <samerphone1994@gmail.com>
2021-05-22 14:12:27 +0200

    cleaned up

--- src/main/java/com/zmp/ServletInitializer.java
+++ src/main/java/com/zmp/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.zmp.app;
+package com.zmp;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
---
+++ src/main/java/com/zmp/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.zmp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping(path = "api/action")
+    public String TestController(){
+        return "action";
+    }
+}

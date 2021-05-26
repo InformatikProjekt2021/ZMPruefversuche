f1cf57a09ed44821c6e4b596ff8a4baba1b97501
samyIO <samerphone1994@gmail.com>
2021-05-26 19:59:33 +0200

    fixed structure

--- src/main/java/com/zmp/ZmpApplication.java
+++ src/main/java/com/zmp/ZmpApplication.java
@@ -2,19 +2,20 @@ package com.zmp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class ZmpApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZmpApplication.class, args);
     }
 
-    @RequestMapping("/hello")
-    public String hello(){
-        return "helloWorld";
+    @RequestMapping("/action")
+    public String action(){
+        return "action";
     }
 
 }

8130375a1b0a524f258a7b63e174b5dc293ada0b
samyIO <samerphone1994@gmail.com>
2021-06-07 12:08:16 +0200

    IP-70 - changed role model and adapted spring security

--- src/main/java/com/zmp/services/UserService.java
+++ src/main/java/com/zmp/services/UserService.java
@@ -2,10 +2,9 @@ package com.zmp.services;
 
 
 import com.zmp.model.dto.User;
-import com.zmp.model.dto.UserRegistrationDto;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 
 public interface UserService extends UserDetailsService{
-    User save(UserRegistrationDto registrationDto);
+    User save(User user);
 }
--- src/main/java/com/zmp/services/UserServiceImpl.java
+++ src/main/java/com/zmp/services/UserServiceImpl.java
@@ -1,12 +1,8 @@
 package com.zmp.services;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.stream.Collectors;
+import java.util.*;
 
-import com.zmp.model.dto.Role;
 import com.zmp.model.dto.User;
-import com.zmp.model.dto.UserRegistrationDto;
 import com.zmp.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -30,11 +26,8 @@ public class UserServiceImpl implements UserService{
     }
 
     @Override
-    public User save(UserRegistrationDto registrationDto) {
-        User user = new User(registrationDto.getFirstName(),
-                registrationDto.getLastName(), registrationDto.getEmail(),
-                passwordEncoder.encode(registrationDto.getPassword()), Arrays.asList(new Role("ROLE_USER")));
-
+    public User save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
@@ -46,11 +39,11 @@ public class UserServiceImpl implements UserService{
             throw new UsernameNotFoundException("Invalid username or password.");
         }
         return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
-                mapRolesToAuthorities(user.getRoles()));
+                mapRolesToAuthorities(user.getRole()));
     }
 
-    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles){
-        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(String role){
+        return Collections.singletonList(new SimpleGrantedAuthority(role));
     }
 
 }

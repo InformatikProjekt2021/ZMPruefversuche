ffc300998342e4f5fb1e9138e108227d67c751f3
samyIO <samerphone1994@gmail.com>
2021-06-10 13:36:55 +0200

    IP-70 - adapted role model

---
+++ src/main/java/com/zmp/repositories/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.zmp.repositories;
+
+import com.zmp.model.dto.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+
+}
